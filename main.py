06554dda2c6ff7f6ddeb03b40a0dc847bf15542c
medhini <medhini95@gmail.com>
2017-12-06 15:50:29 -0600

    Gaussian Pyramid

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import tensorflow as tf
 from scipy import misc
 import matplotlib.pyplot as plt
-
+from gaussian_pyramid import compute_gaussian_pyramid
 
 IN_PATH_A = "images/blurA1.jpg"
 IN_PATH_A_P = "images/rose-src.jpg" 
 IN_PATH_B = "images/blurB1.jpg"
 OUT_PATH_B_P = "images/out.jpg"	 
-
-
-def 
+ 
 def read_image(image_path):
 	im = misc.imread(image_path)
 	# plt.imshow(im)
@@ -17,9 +15,13 @@ def read_image(image_path):
 	return im
 
 def main():
-	A = read_image(IN_PATH_A)
-	A_P = read_image(IN_PATH_A_P)
-	B = read_image(IN_PATH_B)
+	im_a = read_image(IN_PATH_A)
+	im_a_p = read_image(IN_PATH_A_P)
+	im_b = read_image(IN_PATH_B)
+
+	image_pyramid_a = compute_gaussian_pyramid(im_a)
+	image_pyramid_a_p = compute_gaussian_pyramid(im_a_p)
+	image_pyramid_b = compute_gaussian_pyramid(im_b)
 
 if __name__ == '__main__':
    main()
