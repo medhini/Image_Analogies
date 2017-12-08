ab9d06f261fbe54e667869cae932c15cc39a0896
ehelenowski <eryk@helenowski.com>
2017-12-08 15:50:38 -0600

    Add feature concatenation

--- features.py
+++ features.py
@@ -1,4 +1,37 @@
 import colorsys
+import numpy as np
+
+def concat_features(pyramid):
+    features = compute_features(pyramid)
+
+    F = [[]]
+    small_window = 3
+    big_window = 5
+    small_pad = small_window//2
+    big_pad = big_window//2
+
+    for i in range(1,len(pyramid)):
+        fl = np.zeros(features[i].shape+(small_window**2+big_window**2,))
+        small_padded = np.pad(features[i-1], (small_pad,), 'reflect')
+        big_padded = np.pad(features[i], (big_pad,), 'reflect')
+
+        for y in range(features[i].shape[0]):
+            for x in range(features[i].shape[1]):
+                small_x = x//2 + small_pad
+                small_y = y//2 + small_pad
+                big_x = x + big_pad
+                big_y = y + big_pad
+
+                small_patch = small_padded[small_y-small_pad:small_y+small_pad+1, small_x-small_pad:small_x+small_pad+1]
+                big_patch = big_padded[big_y-big_pad:big_y+big_pad+1, big_x-big_pad:big_x+big_pad+1]
+
+                small_vector = small_patch.flatten()
+                big_vector = big_patch.flatten()
+                fl[y,x,:] = np.hstack((big_vector, small_vector))
+        F.append(fl)
+
+    return F
+
 
 def compute_features(pyramid):
     features = []
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from scipy import misc
 import matplotlib.pyplot as plt
 from gaussian_pyramid import compute_gaussian_pyramid
 from features import compute_features
+from features import concat_features
 
 IN_PATH_A = "images/blurA1.jpg"
 IN_PATH_A_P = "images/rose-src.jpg"
@@ -16,21 +17,24 @@ def read_image(image_path):
 
 def main():
 	im_a = read_image(IN_PATH_A)
-	# im_a_p = read_image(IN_PATH_A_P)
-	# im_b = read_image(IN_PATH_B)
+	im_a_p = read_image(IN_PATH_A_P)
+	im_b = read_image(IN_PATH_B)
 
 	image_pyramid_a = compute_gaussian_pyramid(im_a)
-	# image_pyramid_a_p = compute_gaussian_pyramid(im_a_p)
-	# image_pyramid_b = compute_gaussian_pyramid(im_b)
-	features = compute_features(image_pyramid_a)
-	print(features)
+	image_pyramid_a_p = compute_gaussian_pyramid(im_a_p)
+	image_pyramid_b = compute_gaussian_pyramid(im_b)
+
+	# Test to show how to retrieve concatenated features
+	features = concat_features(image_pyramid_a)
+	for i in range(1,len(features)):
+		print(features[i].shape)
 
 
 	"""Computing Luminances"""
 
-	lum_a, lum_a_pyramid, lum_b
-	"""Remap luminance for color artistic images"""
-	im_a, image_pyramid_a = remap_luminance(lum_a, lum_a_pyramid, lum_b)
+	# lum_a, lum_a_pyramid, lum_b
+	# """Remap luminance for color artistic images"""
+	# im_a, image_pyramid_a = remap_luminance(lum_a, lum_a_pyramid, lum_b)
 
 	""""""
 
