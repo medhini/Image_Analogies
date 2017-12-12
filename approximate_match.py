d06d487d06781fffb7c3b1158d2b06d94bcab8e6
jiajenwuuuu <wuzenobia@gmail.com>
2017-12-12 18:07:07 -0500

    add ann

---
+++ approximate_match.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pyflann as pf
+from features import concat_features
+
+
+def ann_index(A_pyramid, Ap_pyramid, level = 6):
+    A_feature = concat_features(A_pyramid)
+    Ap_feature = concat_features(Ap_pyramid)
+
+    flann = [pf.FLANN() for _ in xrange(level)]
+    flann_params = [list([]) for _ in xrange(level)]
+    s = [list([]) for _ in xrange(level)]
+    s_size = [list([]) for _ in xrange(level)]
+
+    for l in range(1, level):
+        print('Building index for level %d / %d' % (l, level-1))
+        
+        s_tmp = []
+        s_tmp.append(np.hstack([A_feature[l], Ap_feature[l]]))
+
+        s[l] = np.vstack(s_tmp)
+        s_size[l] = s[l].shape
+
+        flann_params[l] = flann[l].build_index(s[l], algorithm='kdtree')
+    return flann, flann_params, s, s_size
+
+
+def best_approximate_match(flann, params, BBp_feature):
+    result, dists = flann.nn_index(BBp_feature, 1, checks=params['checks'])
+    return result[0]
+
+
+def px2idx(px, w):
+    rows, cols = px[0], px[1]
+    return (rows * w + cols).astype(int)
+
+def idx2px(idx, w):
+    cols = idx % w
+    rows = (idx-cols) // w
+    return np.array([rows, cols])
