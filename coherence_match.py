b5e46fccc38b3d2c55d3fed773216207177214ea
medhini <medhini95@gmail.com>
2017-12-13 23:09:29 -0600

    Changed coherence

--- coherence_match.py
+++ coherence_match.py
@@ -1,13 +1,12 @@
-from utils import to_1d, Ap_to_1d
+from utils import to_1d
 
-def best_coherence_match(a_prime, (A_h, A_w), BBp_feat, s, im, pixel, Bp_w, n_lg):
+def best_coherence_match(a_prime, (A_h, A_w), BBp_feat, s, im, pixel, n_lg):
     
     assert(len(s) >= 1)
     row, col = pixel
 
     # construct iterables
     rs = []
-    ims = []
     prs = []
     rows = np.arange(np.max([0, row - np.floor(n_lg/2.)]), row + 1, dtype=int)
     cols = np.arange(np.max([0, col - np.floor(n_lg/2.)]), np.min([Bp_w, col + np.floor(n_lg/2.) + 1]), dtype=int)
@@ -21,13 +20,12 @@ def best_coherence_match(a_prime, (A_h, A_w), BBp_feat, s, im, pixel, Bp_w, n_lg
             pr = s[to_1d(r_coord, Bp_w)] + px - r_coord
 
             # i is a list of image nums for each pixel in Bp
-            img_nums = im[to_1d(r_coord, Bp_w)]
+            #img_nums = im[to_1d(r_coord, Bp_w)]
 
             # discard anything outside the bounds of A/Ap lg
             if 0 <= pr[0] < A_h and 0 <= pr[1] < A_w:
                 rs.append(np.array(r_coord))
-                ims.append(img_nums)
-                prs.append(Ap_to_1d(pr, img_nums, A_h, A_w))
+                prs.append(to_1d(pr, A_w))
 
 
     if not rs:
@@ -36,6 +34,6 @@ def best_coherence_match(a_prime, (A_h, A_w), BBp_feat, s, im, pixel, Bp_w, n_lg
 
     rix = np.argmin(norm(As[np.array(prs)] - BBp_feat, ord=2, axis=1))
     r_star = rs[rix]
-    i_star = ims[rix]
+    #i_star = ims[rix]
     # s[r_star] + (q - r-star)
-    return s[to_1d(r_star, Bp_w)] + px - r_star, i_star, r_star
+    return s[to_1d(r_star, Bp_w)] + px - r_star, r_star
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from scipy import misc
+import colorsys
 import matplotlib.pyplot as plt
 import numpy as np
 from gaussian_pyramid import compute_gaussian_pyramid
@@ -13,6 +14,7 @@ IN_PATH_B = "images/blurB1.jpg"
 OUT_PATH_B_P = "images/out.jpg"
 LUM_REMAP = False
 
+max_level = 5  # set the max level of pyramid
 AB_weight = 1  # relative weighting of A and B relative to Ap and Bp
 k    = 0.5     # 0.5 <= k <= 5 for texture synthesis
 n_sm = 3       # coarse scale neighborhood size
@@ -31,17 +33,17 @@ def main():
 	if np.max(im_a_p) > 1.0:
 		im_a_p = im_a_p/255.
 
-	"""Computing Luminances"""
 
-	lum_a, lum_a_pyramid, lum_b = rgb_to_yiq()
 	"""Remap luminance for color artistic images"""
-	im_a, im_a_p = remap_luminance(im_a, im_a_p, im_b)
-	im_a, pyramid_a_p = remap_luminance(lum_a, lum_a_pyramid, lum_b)
+	if LUM_REMAP:
+		im_a, im_a_p = remap_luminance(im_a, im_a_p, im_b)
 
-	pyramid_a = compute_gaussian_pyramid(im_a)
-	pyramid_a_p = compute_gaussian_pyramid(im_a_p)
-	pyramid_b = compute_gaussian_pyramid(im_b)
+	pyramid_a = compute_gaussian_pyramid(im_a, max_level)
+	pyramid_a_p = compute_gaussian_pyramid(im_a_p, max_level)
+	pyramid_b = compute_gaussian_pyramid(im_b, max_level)
 	pyramid_b_p = pyramid_b
+	im_b_yiq = colorsys.rgb_to_yiq(im_b[:,:,0],im_b[:,:,1],im_b[:,:,2])
+	pyramid_color = compute_gaussian_pyramid(im_b_yiq, max_level)
 
 	
 	# Compute features of B
@@ -60,7 +62,6 @@ def main():
 		im_out = np.nan * np.ones((imh, imw, 3))
 
 		s = []
-		im = []
 
 		for row in range(imh):
 			for col in range(imw):
@@ -68,32 +69,27 @@ def main():
 
 				# do something about B and Bp feature
 				feature_b_p = compute_features(pyramid_b_p)
-				small_padded = np.pad(feature_b_p[level-1], (3//2), 'reflect') 
-				big_padded = np.pad(feature_b_p[level], (5//2), 'reflect')
+				small_padded = np.pad(feature_b_p[level-1], (n_sm//2), 'reflect') 
+				big_padded = np.pad(feature_b_p[level], (n_lg//2), 'reflect')
 				BBp_feature = np.hstack([features_b[level][to_1d(px, imw), :],
 									  extract_pixel_feature(small_padded, big_padded, px)])
 
 				assert(BBp_feature.shape == (As_size[level][1],))
-				
 				# Find Approx Nearest Neighbor
 				p_app_ix = best_approximate_match(flann[level], flann_params[level], BBp_feature)
 
-
 				Ap_imh, Ap_imw = im_a_p.shape[:2]
-				p_app, i_app = Ap_to_2d(p_app_ix, Ap_imh, Ap_imw)
-
-				#Coherence match
+				p_app = to_2d(p_app_ix, imw)
 
 				if(len(s)<1):
 					p = p_app
-					i = i_app
 
-				else:   
-					p_coh, i_coh, r_star = best_coherence_match(image_pyramid_a[level], (Ap_imh, Ap_imw), BBp_feature, s, im, px, imw, n_lg)
+				else:
+					#Coherence match   
+					p_coh, r_star = best_coherence_match(image_pyramid_a[level], (Ap_imh, Ap_imw), BBp_feature, s, px, imw, n_lg)
 
 					if np.allclose(p_coh, np.array([-1, -1])):
 						p = p_app
-						i = i_app
 
 					else:
 						AAp_feature_app = As[level][p_app]
@@ -103,19 +99,20 @@ def main():
 
 						if d_coh < d_app * (1 + (2**(level-5)*1)):
 							p = p_coh
-							i = i_coh
 						else:
 							p = p_app
-							i = i_app
 
+				s.append(p)
+				pyramid_b_p[level][row, col] = pyramid_a_p[level][tuple(p)]
 
-				# pyramid_b_p[level][row, col] = pyramid_a_p[i][level][tuple(p)]
+		
+		# Save color output images	
+		im_out_yiq = np.dstack([pyramid_b_p[level], pyramid_color[level][:, :, 1:]])
+		color_im_out = colorsys.yiq_to_rgb(im_out_yiq[:,:,0], im_out_yiq[:,:,1], im_out_yiq[:,:,2])
+		color_im_out = np.clip(color_im_out, 0, 1)
 
-				# s.append(p)
+		plt.imsave('output/level_%d_color.jpg' % level, color_im_out)
 
-
-	
-	""""""
 
 
 if __name__ == '__main__':
